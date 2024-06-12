322b7c288d0e81cdfcda9d593589d73c8de9a4e2
MohdAmaan1802 <mohdamaan1802@gmail.com>
2024-06-12 16:53:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import CoursesSection from "./components/CourseSection";
+import BrowseCategories from "./components/BrowseCategories";
+import CarouselSection from "./components/CarouselSection";
+import Collaborations from "./components/Collaborations";
+import BlendedLearningSection from "./components/BlendedLearningSection";
+import Testimonials from "./components/Testimonials";
+import ContactUs from "./components/ContactUs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <HeroSection />
+      <CoursesSection />
+      <BrowseCategories />
+      <CarouselSection />
+      <Collaborations />
+      <BlendedLearningSection />
+      <Testimonials />
+      <ContactUs />
     </div>
   );
 }
